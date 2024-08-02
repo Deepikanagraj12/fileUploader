9b839c13161152e1cb97509f704fdb8890ed2902
Deepikanagraj12 <deepikanagraj1212@gmail.com>
2024-08-02 13:35:51 +0530

    post middleware added

--- models/File.js
+++ models/File.js
@@ -16,5 +16,32 @@ const fileSchema = new mongoose.Schema({
     }
 })
 
+fileSchema.post("save", async function(doc) {
+    try{
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth:{
+                user : process.env.MAIL_USER,
+                password : process.env.MAIL_PASS
+            }
+        })
+
+        let info = await transporter.sendMail({
+            from:`Deepika`,
+            to: doc.email,
+            subject : `New file uploaded on cloudinary`,
+            html : `<h2>Hello!</h2> <p>File uploaded <a href="${doc.imageURL}"> ${doc.imageURL}</a></p>`
+        })
+    }
+    catch(error){
+        return res.status(400).json({
+            success: false,
+            message:"cannot semd mail"
+        })
+    }
+})
+
+
+
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
